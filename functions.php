95aca9391e691b0528c5d720ca574ece70082194
Al Hassan <shadi.alhassan73@gmail.com>
2020-12-02 14:16:28 +0100

    changes

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function my_style() {
+    wp_enqueue_style(  'style', get_stylesheet_uri()); 
+}
+add_action( 'wp_enqueue_scripts', 'my_style' );
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>
+    
+        Static Title
+    
+    </title>
+    
+    
+    </head>
+    
+    
+    
+    <body>
+    
+    
+    </body>
+
+
+
+
+
+
+
+</html>
