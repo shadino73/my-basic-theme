e2da800be61056c7ba947cf85ea071d64ec015f8
Al Hassan <shadi.alhassan73@gmail.com>
2020-12-03 15:29:30 +0100

    last 03-12-2020

--- functions.php
+++ functions.php
@@ -4,3 +4,18 @@ function my_style() {
     wp_enqueue_style(  'style', get_stylesheet_uri()); 
 }
 add_action( 'wp_enqueue_scripts', 'my_style' );
+
+function register_my_menus() {
+  register_nav_menus(
+    array(
+        'header-menu' => __( 'Kopf Menu' ),
+        'mobile-menu' => __( 'Handy Menu' ),
+        'footer-menu' => __( 'Fuss Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menus' );
+
+
+
+
--- index.php
+++ index.php
@@ -1,22 +1,55 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+    <title><?php bloginfo('title'); ?></title>
+    <?php wp_head() ?>
+
+
+</head>
+
+
+
+<body>
     
-    <title>
+    <nav>
     
-        Static Title
-    
-    </title>
-    
-    
-    </head>
+
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
     
     
+    </nav>
     
-    <body>
-    
-    
-    </body>
+    <main>
+        
+        
+
+    <h1>
+        <?php bloginfo('title'); ?>
+    </h1>
+
+    <?php while( have_posts() ) : the_post(); ?>
+    <h3><?php the_title(); ?></h3>
+    <?php the_content(); ?>
+
+    <?php endwhile; ?>
+
+
+
+    </main>
+    <footer>
+        <p>
+            &copy; 2020 Shadi Al Hassan
+        </p>
+    </footer>
+
+
+
+
+
+
+
+</body>
 
 
 
